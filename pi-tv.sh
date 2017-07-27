5cd71c4c18e587f3ccc071862086bd40ceefc774
Edward <edward@smeathers.co.uk>
2017-07-27 16:23:29 +0100

    Update pi-tv.sh

--- pi-tv.sh
+++ pi-tv.sh
@@ -4,3 +4,11 @@ tar -zxvf media_build.tar.gz
 
 apt-get install raspberrypi-kernel-headers
 apt-get install libproc-processtable-perl
+
+
+#maybe also
+
+apt-get install firmware-linux-nonfree
+# this instead of manual download of firmware from
+# wget linuxtv.org/downloads/firmware/dvb-usb-dib0700-1.20.fw
+# see https://www.raspberrypi.org/forums/viewtopic.php?t=18838&p=185206
